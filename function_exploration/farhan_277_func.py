d209970d39823d2ae62c8a9a3e6607751a6c2855
Mhd. Farhan Lubis <l200220277@student.ums.ac.id>
2022-12-07 16:45:44 +0700

    add function_exploration folder and farhan_277_func.py file to explore functions in python

---
+++ function_exploration/farhan_277_func.py
@@ -0,0 +1,13 @@
+# function/method
+
+# python function syntax
+# def function_name(arguments):
+#     # function body
+#     """ docstring """
+#     instructions
+
+def show_profile(name, nim):
+    return f"Welcome {name}, your student id is {nim}"
+
+
+print(show_profile("Mhd. Farhan lubis", "L200220277"))
