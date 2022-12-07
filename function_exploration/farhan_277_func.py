eb47a9c463bc4b5db94a16c1d7e7943686b3a0c1
Mhd. Farhan Lubis <l200220277@student.ums.ac.id>
2022-12-07 18:24:27 +0700

    make another function in farhan_277_func.py

--- function_exploration/farhan_277_func.py
+++ function_exploration/farhan_277_func.py
@@ -6,8 +6,22 @@
 #     """ docstring """
 #     instructions
 
-def show_profile(name, nim):
-    return f"Welcome {name}, your student id is {nim}"
+# def show_profile(name, nim):
+#     return f"Welcome {name}, your student id is {nim}"
 
 
-print(show_profile("Mhd. Farhan lubis", "L200220277"))
+# print(show_profile("Mhd. Farhan lubis", "L200220277"))
+
+user_name = input("Enter your name (1 word): ").lower()
+user_pin = input("Enter your pin (3 digit): ")
+
+
+def basic_login(name, pin):
+    nim = "L200220277"
+    if user_name == name and user_pin == pin:
+        return f"\nWelcome {name}, your student id is {nim}"
+    else:
+        return "\nYou are not a student of this university"
+
+
+print(basic_login("farhan", "277"))
