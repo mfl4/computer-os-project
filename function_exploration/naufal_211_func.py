75a375f3dca56a7a2ddf38c983942ec398b11b5f
NAUFALZULHM <119654150+NAUFALZULHM@users.noreply.github.com>
2022-12-11 20:24:51 +0700

    Update naufal_211_func.py
    Memperbaiki kesalahan penulisan baris kode

--- function_exploration/naufal_211_func.py
+++ function_exploration/naufal_211_func.py
@@ -1,4 +1,4 @@
-def Koversi_Suhu(C=0, F=0):
+def koversi_suhu(C=0, F=0):
     """Membuat program yang dapat menkonversi suhu
     satuan celcius dan fahrenheit"""
     Cel=(5/9*(F-32))
@@ -7,5 +7,5 @@ def Koversi_Suhu(C=0, F=0):
         print("Suhu",F,"Fahrenheit setara dengan",int(Cel),"Celcius")
     else:
         print("Suhu",C,"Celcius setara dengan",int(Fah),"Fahrenheit")
-Koversi_Suhu(C=9)
-Koversi_Suhu(F=89)
+koversi_suhu(C=9)
+koversi_suhu(F=89)
