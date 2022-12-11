7ee052dd7068bbe4cd2e0e1a51bab54df3c83ae3
Rizky <l200220230@student.ums.ac.id>
2022-12-11 18:08:26 +0700

    Saya menambahkan fungsi penyelesaian dari barisan Aritmatika

--- function_exploration/rizky_230_func.py
+++ function_exploration/rizky_230_func.py
@@ -1,4 +1,4 @@
-def barisanaritmatika(a=0,u2=0,u1=0,n=0):
+def barisan_aritmatika(a=0,u2=0,u1=0,n=0):
     """Membuat penyelesaian dari barisan aritmatika """
     b = int(u2 - u1)
     Un = str(input("Masukan suku :"))
@@ -9,7 +9,7 @@ def barisanaritmatika(a=0,u2=0,u1=0,n=0):
 #note jika ingin mengubah a/u2/u1/n tinggal mengubah sesuai urutannya
 #contoh soal 1
 #Ada suatu barisan aritmatika dengan pola 5,8,11.... Maka berapa suku ke 24?
-barisanaritmatika(5,8,5,24)
+barisan_aritmatika(5,8,5,24)
 #contoh soal 2
 # Ada suatu barisan aritmatika 14,23,52.... Maka berapa suku ke 128?
-barisanaritmatika(14,23,14,128)
+barisan_aritmatika(14,23,14,128)
