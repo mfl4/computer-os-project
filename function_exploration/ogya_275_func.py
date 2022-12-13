d62b6daae1bdc8dd137e84f82cdf8d7a44be4d08
ogya <l200220275@student.ums.ac.id>
2022-12-13 14:30:47 +0700

    saya menambahkan fungsi menampilkan tempat

---
+++ function_exploration/ogya_275_func.py
@@ -0,0 +1,11 @@
+kota, provinsi = 'Solo', 'Jawa Tengah'
+
+def hello ():
+  provinsi = 'Yogyakarta'
+  print(kota, provinsi)
+
+print('Dimana sekarang?')
+hello()
+
+print('\nTampilkan')
+print(kota, provinsi)
