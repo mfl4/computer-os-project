aa5db996650d5a639873bb72b7f7d76c5b931373
NAUFALZULHM <119654150+NAUFALZULHM@users.noreply.github.com>
2022-12-15 09:18:28 +0700

    Update naufal_211_class.py
    correcting errors in writing programs and completing incomplete lines of code

--- ClassExploration/naufal_211_class.py
+++ ClassExploration/naufal_211_class.py
@@ -1,8 +1,10 @@
 class PersegiPanjang(object): 
+    """data dan proses terhadap persegi panjang dengan panjang(pjg) dan lebar(lbr)"""
     def __init__(self,panjang,lebar):
         self.pjg=panjang 
         self.lbr=lebar
-    pass
+        """mengisi property yang tidak punya nilai default"""
+        pass #program diatas tidak ada yg dieksekusi dan langsung melanjutkan program dibawahnya.
 
     def luas(self):
         """mengembalikan luas Persegi Panjang ini"""
