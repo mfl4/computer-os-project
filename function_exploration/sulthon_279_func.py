d38bd7e48f9f34045f6ec57c8758b00d6d38812a
Sulthon Kaffaah Al Farizzi <l200220279@student.ums.ac.idd>
2022-12-22 11:02:32 +0700

    New Function Python Exploration

--- function_exploration/sulthon_279_func.py
+++ function_exploration/sulthon_279_func.py
@@ -1,6 +1,23 @@
-def luas_jajargenjang(a, t):
-     """mengembalikan luas jajar genjang"""
-     L= a*t
-     return L
- 
-print(luas_jajargenjang(12,100))
+# def luas_jajargenjang(a, t):
+#      """mengembalikan luas jajar genjang"""
+#      L= a*t
+#      return L
+# print(luas_jajargenjang(12,100))
+
+print(" ———Toko UMMI———")
+def sumof(price,summ):
+     """fungsi untuk menghitung Total bayar"""
+     return Price*summ
+#input data
+Price= int(input("masukan harga barang: "))
+summ= int(input("masukan jumlah baju yang dibeli: "))
+sumfor=sumof(Price,summ)
+
+#diskon 5% tiap pembelian di atas Rp.100rb
+if summ>100000:
+     summ=summ-0.05*summ
+print("Total Harga = ", "Rp.",summ)
+Pay=int(input("Jumlah Nominal Uang =" ))
+Retrn= (Pay-summ)
+print("Uang Kembalian = ", "Rp.",Retrn)
+
