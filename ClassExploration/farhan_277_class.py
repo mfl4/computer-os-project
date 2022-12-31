db8d0ec4c21ad4a43be4ac1a3937cee864b49660
Mhd. Farhan Lubis <l200220277@student.ums.ac.id>
2022-12-31 10:34:40 +0700

    Update Class Exploration

--- ClassExploration/farhan_277_class.py
+++ ClassExploration/farhan_277_class.py
@@ -1,10 +1,43 @@
-class ClassExploration(object):
-    name = str(input("Enter your name: "))
-
-    def show_title(self):
-        message = f"Welcome to Class Exploration, {name}"
-        return message
+import os
 
 
-ce = ClassExploration()
-print(ce.show_title())
+class Computer:
+    def __init__(self, processor, memory, storage):
+        # self.os = os
+        self.processor = processor
+        self.memory = memory
+        self.storage = storage
+
+    # def get_os_info(self):
+    #     return self.os
+
+    def get_processor_info(self):
+        return self.processor
+
+    def get_memory_info(self):
+        return self.memory
+
+    def get_storage_info(self):
+        return self.storage
+
+
+# Use the os module to get system OS information
+# system_os = os.system('uname -a')
+
+# Create a Computer object with the specified specifications
+pc = Computer('Intel Core i5', '8GB', '1TB')
+
+
+print("My Computer System")
+
+# os_info = pc.get_os_info()
+# print(f'Operating system: {os_info}')
+
+processor_info = pc.get_processor_info()
+print(f'Processor: {processor_info}')
+
+memory_info = pc.get_memory_info()
+print(f'Memory: {memory_info}')
+
+storage_info = pc.get_storage_info()
+print(f'Storage: {storage_info}')
